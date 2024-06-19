96658df6a138861e1d433e3969425dab690a7119
WesleyR10 <wesleyribas2015@gmail.com>
2024-06-18 22:47:44 -0300

    Integration Backend

--- App.tsx
+++ App.tsx
@@ -2,10 +2,11 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { Routes } from '@routes/index';
+import { AuthContextProvider } from '@contexts/AuthContext';
 import { THEME } from './src/theme';
 
 import { Loading } from '@components/Loading';
-import { Routes } from '@routes/index';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,7 +18,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Routes /> : <Loading />}
+      <AuthContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import { useNavigation } from "@react-navigation/native";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
 import { useForm, Controller } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
+import { useAuth } from '@hooks/useAuth';
+
+import { api } from "@services/api";
+
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
+import { AppError } from '@utils/AppError';
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+
 
 type FormDataProps = {
   name: string;
@@ -21,10 +29,14 @@ const signUpSchema = yup.object({
   name: yup.string().required('Informe o nome.'),
   email: yup.string().required('Informe o e-mail').email('E-mail inválido.'),
   password: yup.string().required('Informe a senha').min(6, 'A senha deve ter pelo menos 6 dígitos.'),
-  password_confirm: yup.string().required('Confirme a senha.').oneOf([yup.ref('password')], 'As senhas não conferem.'),
+  password_confirm: yup.string().required('Confirme a senha.').oneOf([yup.ref('password')], 'A confirmação da senha não confere')
 });
 
 export function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toast = useToast();
+  const { singIn } = useAuth();
 
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema),
@@ -36,8 +48,25 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-    console.log({ name, email, password, password_confirm })
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    try {
+      setIsLoading(true)
+
+      await api.post('/users', { name, email, password });
+      await singIn(email, password)
+    } catch (error) {
+      setIsLoading(false);
+
+      const isAppError = error instanceof AppError;
+
+      const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    }
   }
 
   return (
@@ -115,8 +144,8 @@ export function SignUp() {
                 secureTextEntry
                 onChangeText={onChange}
                 value={value}
-                onSubmitEditing={handleSubmit(handleSignUp)} // Enviar pelo teclado
-                returnKeyType="send" // Enviar pelo teclado
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType="send"
                 errorMessage={errors.password_confirm?.message}
               />
             )}
@@ -125,6 +154,7 @@ export function SignUp() {
           <Button
             title="Criar e acessar"
             onPress={handleSubmit(handleSignUp)}
+            isLoading={isLoading}
           />
         </Center>
 
